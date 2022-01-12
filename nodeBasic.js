c9c60960abd1165f657ff45f8d0b489fbfca6f8f
sirsa4 <yaya.abbdi@gmail.com>
2022-01-12 23:18:58 +0100

    node basic. How to run code in terminal

---
+++ nodeBasic.js
@@ -0,0 +1,14 @@
+//first node file
+
+const amount = 12;
+
+if(amount < 10){
+ console.log(`{amount} is less than 10`);
+} else {
+    console.log(`${amount} is more than 10`);
+}
+
+//to run node:
+//open the terminal and type: node file name + enter
+//in here type: node node.js 
+
